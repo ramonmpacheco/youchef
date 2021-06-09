eea79e1de208cea0cdf5a77a514271e823b4a1df
Ramon Pacheco <rmpacheco2@gmail.com>
2021-06-09 06:53:22 -0300

    refact: cat meals screen, add list

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:youchef/data/dummy_data.dart';
 import 'package:youchef/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context).settings.arguments as Category;
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Receitas'),
       ),
-      body: Center(
-        child: Text('Receitas por categoria ${category.id}'),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
       ),
     );
   }
