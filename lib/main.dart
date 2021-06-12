c875d2687989c2d27786583eb3c202be5fc48736
Ramon Pacheco <rmpacheco2@gmail.com>
2021-06-11 22:35:52 -0300

    add: tabs bar

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:youchef/screens/categories_meals_screen.dart';
 import 'package:youchef/screens/categories_screen.dart';
 import 'package:youchef/screens/meal_detail_screen.dart';
+import 'package:youchef/screens/tabs_screen.dart';
 import 'package:youchef/utils/app_routes.dart';
 
 void main() => runApp(YouChefApp());
@@ -25,7 +26,7 @@ class YouChefApp extends StatelessWidget {
             ),
       ),
       routes: {
-        AppRoutes.HOME: (ctx) => CategoriesScreen(),
+        AppRoutes.HOME: (ctx) => TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
         AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
       },
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,20 +5,15 @@ import 'package:youchef/data/dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('You Chef'),
-      ),
-      body: GridView(
-        padding: EdgeInsets.all(25),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20),
-        children:
-            DUMMY_CATEGORIES.map((category) => CategoryItem(category)).toList(),
-      ),
+    return GridView(
+      padding: EdgeInsets.all(25),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
+      children:
+          DUMMY_CATEGORIES.map((category) => CategoryItem(category)).toList(),
     );
   }
 }
