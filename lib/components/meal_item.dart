bd5eefab5ab93d43330cf99a887b15ddcdec99ae
Ramon Pacheco <rmpacheco2@gmail.com>
2021-06-13 20:55:51 -0300

    add: meal favorite btn

--- lib/components/meal_item.dart
+++ lib/components/meal_item.dart
@@ -8,7 +8,18 @@ class MealItem extends StatelessWidget {
   const MealItem(this.meal);
 
   void _selectMeal(BuildContext context) {
-    Navigator.of(context).pushNamed(AppRoutes.MEAL_DETAIL, arguments: meal);
+    Navigator.of(context)
+        .pushNamed(
+      AppRoutes.MEAL_DETAIL,
+      arguments: meal,
+    )
+        .then((value) {
+      if (value == null) {
+        print('Sem Resultado');
+      } else {
+        print('O nome da refeição é $value.');
+      }
+    });
   }
 
   @override
