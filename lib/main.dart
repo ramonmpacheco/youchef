5da0556c290d9640f360e8a80d814c520ca46dea
Ramon Pacheco <rmpacheco2@gmail.com>
2021-06-14 06:58:10 -0300

    add: applying settings filter - wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:youchef/components/settings_screen.dart';
+import 'package:youchef/data/dummy_data.dart';
 import 'package:youchef/screens/categories_meals_screen.dart';
 import 'package:youchef/screens/meal_detail_screen.dart';
 import 'package:youchef/screens/tabs_screen.dart';
 import 'package:youchef/utils/app_routes.dart';
 
+import 'models/meal.dart';
+
 void main() => runApp(YouChefApp());
 
-class YouChefApp extends StatelessWidget {
+class YouChefApp extends StatefulWidget {
+  @override
+  _YouChefAppState createState() => _YouChefAppState();
+}
+
+class _YouChefAppState extends State<YouChefApp> {
+  List<Meal> _availableMeals = DUMMY_MEALS;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,7 +37,8 @@ class YouChefApp extends StatelessWidget {
       ),
       routes: {
         AppRoutes.HOME: (ctx) => TabsScreen(),
-        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) =>
+            CategoriesMealsScreen(_availableMeals),
         AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
         AppRoutes.SETTINGS: (ctx) => SettingsScreen(),
       },
--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:youchef/components/meal_item.dart';
-import 'package:youchef/data/dummy_data.dart';
 import 'package:youchef/models/category.dart';
+import 'package:youchef/models/meal.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
+  final List<Meal> meals;
+  const CategoriesMealsScreen(this.meals);
+
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context).settings.arguments as Category;
-    final categoryMeals = DUMMY_MEALS
-        .where((meal) => meal.categories.contains(category.id))
-        .toList();
+    final categoryMeals =
+        meals.where((meal) => meal.categories.contains(category.id)).toList();
 
     return Scaffold(
       appBar: AppBar(
